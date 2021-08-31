4d7a73ee466cfe581cdb84068eac033b42db172f
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-08-31 17:43:10 +0200

    Tag using base64 then upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,100 @@
 <template>
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <input
-      type="file"
-      name="image"
-      id="image"
-      required
-      class="
-        focus:ring-indigo-500
-        focus:border-indigo-500
-        flex-1
-        block
-        w-full
-        rounded-none rounded-r-md
-        sm:text-sm
-        border-gray-300
-      "
-      @change="upload"
-    />
-    <div v-if="uploading">Uploading...</div>
-    <div v-if="error">{{ error }}</div>
+    <div class="shadow m-10 text-center p-5">
+      <h1 class="m-5 text-lg">Select image to tag</h1>
+      <p class="m-5 text-sm">
+        This utility will automatically tag the image based on it's contents
+      </p>
+      <input
+        type="file"
+        name="image"
+        id="image"
+        required
+        accept="image/*"
+        @change="handle"
+      />
+      <div class="text-sm text-yellow-600" v-if="tagging">Tagging...</div>
+      <div class="text-sm text-yellow-600" v-if="uploading">Uploading...</div>
+      <div class="text-sm text-red-500" v-if="error">{{ error }}</div>
+    </div>
+
+    <section class="mt-8 pb-16">
+      <ul
+        role="list"
+        class="
+          grid grid-cols-2
+          gap-x-4 gap-y-8
+          sm:grid-cols-3
+          sm:gap-x-6
+          md:grid-cols-4
+          lg:grid-cols-3
+          xl:grid-cols-4
+          xl:gap-x-8
+        "
+      >
+        <li v-for="(image, idx) in images" :key="idx" class="relative">
+          <div
+            class="
+              focus-within:ring-2
+              focus-within:ring-offset-2
+              focus-within:ring-offset-gray-100
+              focus-within:ring-indigo-500
+              group
+              block
+              w-full
+              aspect-w-10 aspect-h-7
+              rounded-lg
+              bg-gray-100
+              overflow-hidden
+            "
+          >
+            <img
+              :src="`${image.secure_url}?ap=em`"
+              :alt="`Image ${image.public_id}`"
+              class="group-hover:opacity-75 object-cover pointer-events-none"
+            />
+          </div>
+          <p
+            class="
+              mt-2
+              block
+              text-sm
+              font-medium
+              text-gray-900
+              truncate
+              pointer-events-none
+            "
+          >
+            {{ image.public_id }}
+          </p>
+          <p
+            class="block text-sm font-medium text-gray-500 pointer-events-none"
+          >
+            {{ image.bytes }} bytes
+          </p>
+          <div>
+            <span
+              class="
+                mr-2
+                inline-flex
+                items-center
+                px-2.5
+                py-0.5
+                rounded-full
+                text-xs
+                font-medium
+                bg-gray-100
+                text-gray-800
+              "
+              v-for="(tag, idx) in image.tags"
+              :key="idx"
+            >
+              {{ tag }}
+            </span>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -26,101 +102,28 @@
 export default {
   data() {
     return {
+      tagging: false,
       uploading: false,
       error: false,
-      images: [
-        {
-          asset_id: "e27378401b77573969debb652b0b9303",
-          public_id: "nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug",
-          version: 1630413200,
-          version_id: "dcc81c459b7cb4f768101689a2b8aec2",
-          signature: "6a072342985e8b404cc84c9ffff951a25cc3cef2",
-          width: 1280,
-          height: 960,
-          format: "jpg",
-          resource_type: "image",
-          created_at: "2021-08-31T12:33:20Z",
-          tags: [],
-          bytes: 150613,
-          type: "upload",
-          etag: "c89f89f3bf2b657f56de8268b61720b1",
-          placeholder: false,
-          url: "http://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          secure_url:
-            "https://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          access_mode: "public",
-        },
-        {
-          asset_id: "e27378401b77573969debb652b0b9303",
-          public_id: "nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug",
-          version: 1630413200,
-          version_id: "dcc81c459b7cb4f768101689a2b8aec2",
-          signature: "6a072342985e8b404cc84c9ffff951a25cc3cef2",
-          width: 1280,
-          height: 960,
-          format: "jpg",
-          resource_type: "image",
-          created_at: "2021-08-31T12:33:20Z",
-          tags: [],
-          bytes: 150613,
-          type: "upload",
-          etag: "c89f89f3bf2b657f56de8268b61720b1",
-          placeholder: false,
-          url: "http://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          secure_url:
-            "https://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          access_mode: "public",
-        },
-        {
-          asset_id: "e27378401b77573969debb652b0b9303",
-          public_id: "nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug",
-          version: 1630413200,
-          version_id: "dcc81c459b7cb4f768101689a2b8aec2",
-          signature: "6a072342985e8b404cc84c9ffff951a25cc3cef2",
-          width: 1280,
-          height: 960,
-          format: "jpg",
-          resource_type: "image",
-          created_at: "2021-08-31T12:33:20Z",
-          tags: [],
-          bytes: 150613,
-          type: "upload",
-          etag: "c89f89f3bf2b657f56de8268b61720b1",
-          placeholder: false,
-          url: "http://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          secure_url:
-            "https://res.cloudinary.com/hackit-africa/image/upload/v1630413200/nuxtjs-image-classification/rs3ak4vdkdlp9ylsvnug.jpg",
-          access_mode: "public",
-        },
-      ],
+      images: [],
     };
   },
   methods: {
-    async upload(e) {
-      this.uploading = true;
-      // Instructor video file
+    async handle(e) {
       let file = e.target.files[0];
-      /* Read data */
-      const data = await this.readData(file);
-      /* upload the converted data */
-      const resp = await this.$cloudinary.upload(data, {
-        upload_preset: "default-preset",
-        folder: "nuxtjs-image-classification",
-      });
-      this.label(resp, data);
-      this.uploading = false;
+
+      const fileData = await this.readData(file);
+
+      this.getTags(fileData);
     },
-    async label(image, imageData) {
-      const url = this.$cloudinary.image.url(image.public_id);
+
+    async getTags(fileData) {
+      this.tagging = true;
 
       const data = {
         requests: [
           {
-            image: {
-              source: {
-                imageUri: url,
-              },
-            },
+            image: { content: fileData.split(",")[1] },
             features: [
               {
                 maxResults: 5,
@@ -137,6 +140,7 @@ export default {
       );
 
       if (resp.responses[0].error) {
+        this.tagging = false;
         this.error = resp.responses[0].error.message;
         return;
       }
@@ -147,13 +151,23 @@ export default {
         (label) => label.description
       );
 
-      const uploadResp = await this.$cloudinary.upload(imageData, {
+      this.upload(fileData, tags);
+
+      this.tagging = false;
+    },
+
+    async upload(fileData, tags) {
+      this.uploading = true;
+
+      const uploadResp = await this.$cloudinary.upload(fileData, {
         upload_preset: "default-preset",
         folder: "nuxtjs-image-classification/tagged",
         tags,
       });
 
       this.images.push(uploadResp);
+
+      this.uploading = false;
     },
     readData(f) {
       return new Promise((resolve) => {
